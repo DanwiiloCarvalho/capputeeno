2bcca7f9c73120c5382d47c2652edd25310d43fc
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-01-16 17:43:04 -0300

    feat: create global style and theme

---
+++ src/app/globalStyle.ts
@@ -0,0 +1,28 @@
+'use client'
+
+import { createGlobalStyle } from 'styled-components'
+
+export const defaultTheme = {
+    '--Background-Primary': '#F0F0F5',
+    '--Background-Secondary': '#FFF',
+    '--Brand-Name': '#5D5D6D',
+    '--Price-Color': '#09090A',
+    '--Cart-Quantity': '#DE3838',
+    '--Finalize-Purchase': '#51B853',
+    '--Trash-Color': '#DE3838'
+} as const
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+    }
+
+    body, html {
+        max-width: 100vw;
+        overflow-x: hidden;
+        -webkit-font-smoothing: antialiased;
+    }
+`
