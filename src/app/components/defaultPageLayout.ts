fd8ffdf28d7697e9aa0bc44c12905876858cb2fe
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-01-30 16:30:49 -0300

    feat: add new media queries to DefaultPageLayout

--- src/app/components/defaultPageLayout.ts
+++ src/app/components/defaultPageLayout.ts
@@ -8,6 +8,37 @@ export const DefaultPageLayout = styled.div`
     padding: 0.75rem 1.5rem;
 
     @media screen and (min-width: 768px) {
-        padding: 2.12rem 10rem 3.75rem;
+        //padding: 2.12rem 5rem 3.75rem;
+        padding-top: 1.66rem;
     }
+
+    @media screen and (min-width: 960px) {
+        //padding: 2.12rem 5rem 3.75rem;
+        padding: 2.12rem 1rem 3.75rem;
+    }
+
+    @media screen and (min-width: 992px) {
+        //padding: 2.12rem 5rem 3.75rem;
+        padding: 2.12rem 2rem 3.75rem;
+    }
+
+    @media screen and (min-width: 1140px) {
+        //padding: 2.12rem 5rem 3.75rem;
+        padding: 2.12rem 4rem 3.75rem;
+    }
+
+    @media screen and (min-width: 1200px) {
+        //padding: 2.12rem 10rem 3.75rem;
+        padding: 2.12rem 8rem 3.75rem;
+    }
+
+    @media screen and (min-width: 1920px) {
+        //padding: 2.12rem 20rem 3.75rem;
+        padding: 4rem 30rem 3.75rem;
+    }
+
+    @media screen and (min-width: 2560px) {
+        padding: 5rem 40rem 3.75rem;
+    }
+
 `
