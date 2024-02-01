22c8d01d0c6359dec09c0d4a1552007d9e70553c
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-02-01 15:24:12 -0300

    feat: create CartItem component

---
+++ src/app/cart/components/cart-item/index.tsx
@@ -0,0 +1,25 @@
+import mugImage from '../../../product-image/caneca.jpg'
+import Image from 'next/image'
+import { CartItemContainer, QuantityPriceContainer, QuantityTrigger } from './styles'
+
+export function CartItem() {
+    return (
+        <CartItemContainer>
+            <Image src={mugImage} alt='' />
+            <div>
+                <div>
+                    <h3>Caneca de cerâmica rústica</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab itaque voluptas voluptatem neque ipsam dolorum fugiat autem deserunt vel saepe natus, ullam error explicabo tempora corrupti et voluptatum enim tenetur dicta aut atque incidunt quidem? Illum, voluptatem earum neque, quia saepe repudiandae laboriosam animi temporibus odit quis, harum qui. Magnam totam, reprehenderit et beatae eius molestiae alias ex. Rem natus reiciendis ipsa repudiandae accusantium dolor iste recusandae porro illo, veritatis beatae quis soluta perspiciatis sint sit aliquam voluptas similique velit voluptate quas temporibus molestiae quae. Aliquam a, veniam rem culpa dolorem, ducimus porro sapiente voluptates voluptatum dignissimos dolores adipisci nisi?</p>
+                </div>
+                <QuantityPriceContainer>
+                    <QuantityTrigger name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </QuantityTrigger>
+                    <span>R$ 40,00</span>
+                </QuantityPriceContainer>
+            </div>
+        </CartItemContainer>
+    )
+}
---
+++ src/app/cart/components/cart-item/styles.ts
@@ -0,0 +1,78 @@
+'use client'
+
+import styled from "styled-components"
+
+export const CartItemContainer = styled.article`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: inherit;
+    border-radius: 0.5rem;
+    background-color: ${ props => props.theme["--Background-Secondary"] };
+
+    > img {
+        width: 100%;
+        height: auto;
+        border-top-left-radius: inherit;
+        border-top-right-radius: inherit;
+    }
+
+
+    > div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 1rem 1rem;
+        flex: 1;
+        gap: 1.56rem;
+
+        > div {
+            color: ${ props => props.theme["--Selected-Category-Filter"] };
+
+            h3 {
+                font-size: 1.25rem;
+                font-weight: 300;
+                line-height: 1.875rem;
+            }
+
+            p {
+                margin-top: .75rem;
+                font-size: .75rem;
+                font-weight: 400;
+                line-height: 1.125rem;
+
+                display: -webkit-box;
+                -webkit-line-clamp: 4;
+                overflow: hidden;
+                -webkit-box-orient: vertical;
+            }
+        }
+    }
+`
+
+export const QuantityTrigger = styled.select`
+    border-radius: .5rem;
+    border: 1px solid ${ props => props.theme["--Select-Border"] };
+    background-color: ${ props => props.theme["--Select-Background"] };
+    padding: .5rem .25rem .5rem .75rem;
+    color: ${ props => props.theme["--Text-Dark-Inputs-Icons"] };
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5rem;
+    
+    width: 4rem;
+`
+
+export const QuantityPriceContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    //margin-top: 1.56rem;
+
+    span {
+        color: ${ props => props.theme["--Price-Color"] };
+        font-weight: 600;
+        font-size: 1rem;
+        line-height: 1.5rem;
+        align-self: flex-end;
+    }
+`
