1de550209e8b275e10e13cdb88f0d4f05326a3b8
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-02-14 18:22:10 -0300

    feat: add priority attribute to Image tag and fix priceformat function import

--- src/app/components/product-card/index.tsx
+++ src/app/components/product-card/index.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import imageExample from "@/app/product-image/caneca.jpg"
 import { ProductCardContainer, Separator } from "./styles";
-import { priceFormat } from "@/lib/price-format";
-
+import { priceFormat } from "@/utils/price-format";
 interface ProductCardProps {
     image_url: string,
     name: string,
@@ -12,7 +11,7 @@ interface ProductCardProps {
 export function ProductCard({ image_url, name, price_in_cents }: ProductCardProps) {
     return (
         <ProductCardContainer>
-            <Image src={image_url} alt="" width={imageExample.width} height={imageExample.height} />
+            <Image src={image_url} alt="" width={imageExample.width} height={imageExample.height} priority />
             <div>
                 <h2>{name}</h2>
                 <Separator />
--- src/app/components/product-card/styles.ts
+++ src/app/components/product-card/styles.ts
@@ -5,10 +5,6 @@ import styled from "styled-components"
 export const ProductCardContainer = styled.article`
     display: flex;
     flex-direction: column;
-    
-    /* width: 16rem; */
-    /* height: 23.625rem; */
-
     line-height: 1.3125rem;
     border-radius: 0.5rem 0.5rem 0.25rem 0.25rem;
     background-color: rgba(255, 255, 255, 0.40);
@@ -25,6 +21,7 @@ export const ProductCardContainer = styled.article`
         height: 18.75rem;
         border-top-left-radius: inherit;
         border-top-right-radius: inherit;
+        object-fit: cover;
     }
 
     > div {
