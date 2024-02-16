ca60ae07cd8bafff94497bca28bf9b74e833476c
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-02-16 11:14:51 -0300

    feat: generate static product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,31 +1,82 @@
-import imageExample from "@/app/product-image/caneca.jpg"
 import Image from "next/image"
 import { Description, Info, InfoSection, ProductPageContainer } from "./styles"
 import { BackButton } from "@/app/components/back-button"
 import Link from "next/link"
 import { AddToCartButton } from "./components/add-to-cart-button"
+import { priceFormat } from "@/utils/price-format"
+import { ProductList } from "@/types/product-list"
+import { useProduct } from "@/hooks/useProduct"
+interface ProductPageProps {
+    params: {
+        id: string
+    }
+}
 
-export default function ProductPage(){
-    return(
+const url = process.env.NEXT_PUBLIC_API_URL as string
+
+export async function generateStaticParams() {
+
+    const queryText = `
+    {
+        allProducts {
+          id
+          name
+          category
+          price_in_cents
+          description
+        }
+    }
+    `
+
+    const request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            query: queryText
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    try {
+        const response = await fetch(request)
+        const products: ProductList = await response.json()
+        return products.data.allProducts.map(product => ({ id: product.id }))
+    } catch (error) {
+        console.error("GenerateStaticParams error: " + error)
+    }
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+    const { id } = params
+
+    const { data } = await useProduct(url, id)
+
+    return (
         <ProductPageContainer>
             <Link href={'/'}>
                 <BackButton />
             </Link>
             <InfoSection>
-                <Image src={imageExample} alt="" width={640} height={580} />
-                <Info>
-                    <div>
-                        <span>Caneca</span>
-                        <h2>Caneca de cerâmica rústica</h2>
-                        <span>R$ 40,00</span>
-                        <p>*Frete de R$40,00 para todo o Brasil. Grátis para compras acima de R$900,00.</p>
-                        <Description>
-                            <h3>Descrição</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe harum, dolor iusto ipsum, ipsa quas beatae dolores alias necessitatibus ratione laborum nihil facere quos molestias temporibus ex iste, nostrum non? Aliquam, in incidunt? Libero fugit, unde rem excepturi quaerat alias voluptates? Beatae saepe sint debitis repellendus amet ipsam porro at rerum! Nemo voluptas officia ullam cupiditate soluta atque explicabo. Accusantium perferendis voluptatibus necessitatibus. Provident asperiores architecto cupiditate voluptates aliquid vero! Hic doloribus quasi recusandae quibusdam deleniti ipsa aperiam nemo provident blanditiis iure laborum aliquid fuga ab laboriosam, illo dignissimos veniam perspiciatis explicabo beatae necessitatibus! Laboriosam, doloribus, rerum facere magni unde libero ipsam necessitatibus voluptates et mollitia sit eveniet labore! Labore, nemo molestias doloribus ut officia dicta aliquam nobis voluptate amet fugit. Suscipit quae, illo cupiditate, molestiae distinctio a at excepturi placeat culpa provident necessitatibus ab. Odit fuga numquam sed facilis itaque quis modi, ad cupiditate quibusdam doloribus autem non aliquam totam ex voluptates vero. Sunt animi rerum earum, delectus dignissimos beatae soluta iusto accusantium in expedita exercitationem veritatis quas sit. Nobis explicabo iusto quam deserunt! Praesentium aperiam ipsa magnam itaque, hic quisquam ex. Dicta, nisi ea sed totam aliquid, excepturi esse praesentium sunt nemo suscipit ipsam magni ullam ipsum reprehenderit.</p>
-                        </Description>
-                    </div>
-                    <AddToCartButton />
-                </Info>
+                {
+                    data &&
+                    <>
+                        <Image src={data?.data.Product.image_url as string} alt="" width={640} height={580} />
+                        <Info>
+                            <div>
+                                <span>{data.data.Product.category}</span>
+                                <h2>{data.data.Product.name}</h2>
+                                <span>{priceFormat(data.data.Product.price_in_cents)}</span>
+                                <p>*Frete de R$40,00 para todo o Brasil. Grátis para compras acima de R$900,00.</p>
+                                <Description>
+                                    <h3>Descrição</h3>
+                                    <p>{data.data.Product.description}</p>
+                                </Description>
+                            </div>
+                            <AddToCartButton />
+                        </Info>
+                    </>
+                }
             </InfoSection>
         </ProductPageContainer>
     )
