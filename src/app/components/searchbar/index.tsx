1ffe1a22f6bb43ef791375116b136cf9b86e4396
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-02-19 18:48:23 -0300

    feat: install use-debounce package and use it to ensure delay after write in search bar

--- src/app/components/searchbar/index.tsx
+++ src/app/components/searchbar/index.tsx
@@ -1,10 +1,19 @@
+'use client'
+import { AppContext, AppContextProviderProps } from "@/app/contexts/context-provider";
+import { useContext } from "react";
+import { useDebouncedCallback } from "use-debounce";
 import { SearchLoupe } from "../icons/search-loupe";
 import { SearchBarContainer } from "./styles";
 
 export function SearchBar() {
+    const { setSearchBarValue } = useContext(AppContext) as AppContextProviderProps
+    const handleInputSearchBar = useDebouncedCallback((value) => {
+        setSearchBarValue(value)
+    }, 500)
+    
     return (
         <SearchBarContainer>
-            <input type="text" placeholder="Procurando por algo específico?" />
+            <input onChange={(e) => handleInputSearchBar(e.target.value)} type="text" placeholder="Procurando por algo específico?" />
             <SearchLoupe />
         </SearchBarContainer>
     )
