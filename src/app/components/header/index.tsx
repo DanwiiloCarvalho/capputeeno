32e4ee586d5228dcd2b262cabd78abfbea7dc04e
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-02-27 19:50:31 -0300

    feat: remove CartCount component from Header and add it to CartIcon component

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,8 +3,10 @@ import { HeaderContainer } from "./styles"
 import { CartIcon } from "../icons/cart-icon"
 import { SearchBar } from "../searchbar"
 import { CartCount } from "../icons/cart-count"
+import { useLocalStorage } from "@/hooks/useLocalStorage"
 
 export function Header(){
+
     return(
         <HeaderContainer>
             <Link href={'/'} prefetch={false}>capputeeno</Link>
@@ -12,7 +14,6 @@ export function Header(){
                 <SearchBar />
                 <Link href={'/cart'} prefetch={false}>
                     <CartIcon />
-                    {<CartCount />}
                 </Link>
             </div>
         </HeaderContainer>
