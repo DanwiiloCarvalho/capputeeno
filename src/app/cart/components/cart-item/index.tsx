ddfd1166e8626de603b9ca3f125436e13c563410
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-02-27 23:40:12 -0300

    feat: map products list and show all products in cart page

--- src/app/cart/components/cart-item/index.tsx
+++ src/app/cart/components/cart-item/index.tsx
@@ -1,28 +1,35 @@
-import mugImage from '../../../product-image/caneca.jpg'
 import Image from 'next/image'
-import { CartItemContainer, QuantityPriceContainer, QuantityTrigger } from './styles'
-import { TrashIcon } from '@/app/components/icons/trash-icon'
+import mugImage from '../../../product-image/caneca.jpg'
 import { TrashButton } from '../trash-button'
+import { CartItemContainer, QuantityPriceContainer, QuantityTrigger } from './styles'
+import { priceFormat } from '@/utils/price-format'
+import { CartProductItem } from '@/types/cart-product-item'
 
-export function CartItem() {
+export function CartItem(cartItem: CartProductItem) {
+    let options = []
+    for (let option = 1; option <= cartItem.units; option++) {
+        options.push(<option key={option} value={option}>{option}</option>)
+    }
+
     return (
         <CartItemContainer>
-            <Image src={mugImage} alt='' />
+            <Image src={cartItem.image_url} alt='' width={256} height={211} />
             <div>
                 <div>
                     <div>
-                        <h3>Caneca de cerâmica rústica</h3>
+                        <h3>{ cartItem.name }</h3>
                         <TrashButton />
                     </div>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab itaque voluptas voluptatem neque ipsam dolorum fugiat autem deserunt vel saepe natus, ullam error explicabo tempora corrupti et voluptatum enim tenetur dicta aut atque incidunt quidem? Illum, voluptatem earum neque, quia saepe repudiandae laboriosam animi temporibus odit quis, harum qui. Magnam totam, reprehenderit et beatae eius molestiae alias ex. Rem natus reiciendis ipsa repudiandae accusantium dolor iste recusandae porro illo, veritatis beatae quis soluta perspiciatis sint sit aliquam voluptas similique velit voluptate quas temporibus molestiae quae. Aliquam a, veniam rem culpa dolorem, ducimus porro sapiente voluptates voluptatum dignissimos dolores adipisci nisi?</p>
+                    <p>{ cartItem.description }</p>
                 </div>
                 <QuantityPriceContainer>
-                    <QuantityTrigger name="" id="">
-                        <option value="1">1</option>
+                    <QuantityTrigger name="" id="" defaultValue={cartItem.units}>
+                        {/* <option value="1">1</option>
                         <option value="2">2</option>
-                        <option value="3">3</option>
+                        <option value="3">3</option> */}
+                        { options.map(option => option) }
                     </QuantityTrigger>
-                    <span>R$ 40,00</span>
+                    <span>{ priceFormat(cartItem.price_in_cents) }</span>
                 </QuantityPriceContainer>
             </div>
         </CartItemContainer>
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Link from "next/link";
 import { BackButton } from "../components/back-button";
 import { CartPageContainer, CartProductListContainer } from "./styles";
 import { CartItem } from "./components/cart-item";
 import { OrderSummary } from "./components/order-summary";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 export default function CartPage() {
+
+    const { products } = useLocalStorage()
+
     return (
         <CartPageContainer>
             <div>
@@ -15,10 +20,7 @@ export default function CartPage() {
                     <h2>Seu carrinho</h2>
                     <p>Total (3 produtos) <span>R$161,00</span></p>
                     <ul>
-                        <li><CartItem /></li>
-                        <li><CartItem /></li>
-                        <li><CartItem /></li>
-                        <li><CartItem /></li>
+                        { products.map(cartItem => <li key={cartItem.id}><CartItem {...cartItem} /></li>) }
                     </ul>
                 </CartProductListContainer>
             </div>
