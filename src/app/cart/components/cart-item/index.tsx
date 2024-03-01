18238cab8345646a33fb83ff0afc02b8a3aaa7cf
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-03-01 17:55:47 -0300

    feat: show increasing values up to the values of units of a products in the cart.

--- src/app/cart/components/cart-item/index.tsx
+++ src/app/cart/components/cart-item/index.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image'
-import mugImage from '../../../product-image/caneca.jpg'
 import { TrashButton } from '../trash-button'
 import { CartItemContainer, QuantityPriceContainer, QuantityTrigger } from './styles'
 import { priceFormat } from '@/utils/price-format'
 import { CartProductItem } from '@/types/cart-product-item'
 
 export function CartItem(cartItem: CartProductItem) {
-    let options = []
+    let unitsOptions = []
     for (let option = 1; option <= cartItem.units; option++) {
-        options.push(<option key={option} value={option}>{option}</option>)
+        unitsOptions.push(<option key={option} value={option}>{option}</option>)
     }
 
     return (
@@ -18,16 +17,13 @@ export function CartItem(cartItem: CartProductItem) {
                 <div>
                     <div>
                         <h3>{ cartItem.name }</h3>
-                        <TrashButton />
+                        <TrashButton cartItemId={cartItem.id} />
                     </div>
                     <p>{ cartItem.description }</p>
                 </div>
                 <QuantityPriceContainer>
                     <QuantityTrigger name="" id="" defaultValue={cartItem.units}>
-                        {/* <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option> */}
-                        { options.map(option => option) }
+                        { unitsOptions.map(option => option) }
                     </QuantityTrigger>
                     <span>{ priceFormat(cartItem.price_in_cents) }</span>
                 </QuantityPriceContainer>
