447e2075c32f0c5deeee6cf16f40b37b76d43741
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-06-07 18:34:47 -0300

    refactor: remove unnecessary console.log from useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -62,7 +62,6 @@ export function useLocalStorage() {
     }
 
     function updateUnits(productId: string, unitsValue: number) {
-        console.log("Função chamada!")
         const newProductsList = products.map(product => {
             if (product.id === productId) {
                 product.units = unitsValue
