caf0d8056bedba2bb3ca2842682ce6e8c45684b3
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-07-24 19:53:25 -0300

    feat: add a skeleton screen using Chakra to the ProductList component

--- src/app/components/product-list/index.tsx
+++ src/app/components/product-list/index.tsx
@@ -2,6 +2,7 @@
 import { AppContext, AppContextProviderProps } from "@/app/contexts/context-provider";
 import { useProducts } from "@/hooks/useProducts";
 import { useSearchBar } from "@/hooks/useSearchBar";
+import { Box, Grid, GridItem, Skeleton } from '@chakra-ui/react';
 import Link from "next/link";
 import { useContext } from "react";
 import { ProductCard } from "../product-card";
@@ -9,29 +10,76 @@ import { ProductListContainer } from "./styles";
 
 export function ProductList() {
     const { categoryValue, priorityValue, searchBarValue } = useContext(AppContext) as AppContextProviderProps
-    const { data } = useProducts(categoryValue, priorityValue)
+    const { data, isLoading } = useProducts(categoryValue, priorityValue)
 
     const { filteredValue } = useSearchBar(data, searchBarValue)
 
     return (
-        <ProductListContainer>
-            <ul>
-                {
-                    filteredValue?.map(product => {
-                        return (
-                            <li key={product.id}>
-                                <Link href={`/product/${product.id}`} prefetch={false}>
-                                    <ProductCard
-                                        image_url={product.image_url}
-                                        name={product.name}
-                                        price_in_cents={product.price_in_cents}
+        <>
+            {
+                isLoading ? (
+                    <Grid
+                        display={"grid"}
+                        justifyContent={"center"}
+                        gridTemplateColumns={"repeat(auto-fill, 16rem)"}
+                        gridTemplateRows={"repeat(auto-fit, 23.625rem)"}
+                        gridRowGap={"1.5rem"}
+                        gridColumnGap={"2rem"}
+                        marginTop={"2rem"}
+                    >
+                        {Array.from({ length: 60 }).map((_, index) => (
+                            <GridItem key={index}>
+                                <Box 
+                                    height="100%" 
+                                    boxShadow="lg" 
+                                    bg="white" 
+                                    borderRadius="0.5rem 0.5rem 0.25rem 0.25rem"
+                                    display='flex'
+                                    flexDirection="column"
+                                >
+                                    <Skeleton 
+                                        height="18.75rem" 
+                                        width="100%" 
+                                        mb="2" 
+                                        borderTopLeftRadius="inherit"
+                                        borderTopRightRadius="inherit"
                                     />
-                                </Link>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </ProductListContainer>
+                                    <Box 
+                                        display="flex" 
+                                        flexDirection="column" 
+                                        gap="0.5rem"
+                                        flex="1" 
+                                        padding="0.5rem 0.75rem"
+                                    >
+                                        <Skeleton height="1rem" width="100%" />
+                                        <Skeleton height="1rem" width="100%" />
+                                    </Box>
+                                </Box>
+                            </GridItem>
+                        ))}
+                    </Grid>
+                ) : (
+                    <ProductListContainer>
+                        <ul>
+                            {
+                                filteredValue?.map(product => {
+                                    return (
+                                        <li key={product.id}>
+                                            <Link href={`/product/${product.id}`} prefetch={false}>
+                                                <ProductCard
+                                                    image_url={product.image_url}
+                                                    name={product.name}
+                                                    price_in_cents={product.price_in_cents}
+                                                />
+                                            </Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </ProductListContainer>
+                )
+            }
+        </>
     )
 }
