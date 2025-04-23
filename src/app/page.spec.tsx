48315a28081e9d133fd9cd7ccbe4293f9a2bec6e
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-04-23 18:56:23 -0300

    test: create test for the Home component

---
+++ src/app/page.spec.tsx
@@ -0,0 +1,58 @@
+import { render } from "@testing-library/react"
+import Home from "./page"
+import { Category } from "@/enum/category";
+import { Priority } from "@/enum/priority";
+import { AppContext } from "./contexts/context-provider";
+import { Mock } from "vitest";
+import { useProducts } from "@/hooks/useProducts";
+import { useSearchBar } from "@/hooks/useSearchBar";
+
+const mockProductList = [
+    {
+        category: "t-shirts",
+        name: "Camiseta Broken Saints",
+        price_in_cents: 3562,
+        sales: 10,
+        id: "1",
+        image_url: "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-03.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate fuga, nulla voluptatum consequatur veniam delectus repellat. Molestias quam fugiat ad?"
+    },
+    {
+        category: "mugs",
+        name: "Caneca The Grounds",
+        price_in_cents: 4230,
+        sales: 5,
+        id: "2",
+        image_url: "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-03.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate fuga, nulla voluptatum consequatur veniam delectus repellat. Molestias quam fugiat ad?"
+    }
+]
+
+vi.mock('@/hooks/useProducts');
+vi.mock('@/hooks/useSearchBar');
+
+describe('Home component', () => {
+    it('should render the Home component', async () => {
+
+        const contextValues = {
+            categoryValue: Category.MUGS,
+            priority: Priority.BEST_SELLERS,
+            searhBarValue: ''
+        };
+
+        (useProducts as Mock).mockReturnValue({
+            data: mockProductList,
+            isLoading: true
+        });
+
+        (useSearchBar as Mock).mockReturnValue({
+            filteredValue: mockProductList
+        })
+
+        render(
+            <AppContext.Provider value={{ ...contextValues }}>
+                <Home />
+            </AppContext.Provider>
+        )
+    })
+})
