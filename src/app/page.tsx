4711aa94223c5f79ff06581001f18a71c6084c9c
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-04-29 00:08:13 -0300

    feat: add Pagination component to the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+'use client'
+import { useContext } from "react";
 import { FilterBar } from "./components/filter-bar";
+import { Pagination } from "./components/pagination";
 import { ProductList } from "./components/product-list";
 import { MainContainer } from "./styles";
+import { AppContext, AppContextProviderProps } from "./contexts/context-provider";
+import { Category } from "@/enum/category";
 
 export default function Home() {
-  
+  const { products, categoryValue, currentPage } = useContext(AppContext) as AppContextProviderProps
+
   return (
     <MainContainer>
       <FilterBar />
+      {
+        products && (categoryValue === Category.ALL_PRODUCTS) ? 
+        <Pagination currentPage={currentPage} totalPages={5} /> :
+        <Pagination currentPage={currentPage} totalPages={3} />
+      }
       <ProductList />
+      {
+        products && (categoryValue === Category.ALL_PRODUCTS) ? 
+        <Pagination currentPage={currentPage} totalPages={5} /> :
+        <Pagination currentPage={currentPage} totalPages={3} />
+      }
     </MainContainer>
   )
 }
