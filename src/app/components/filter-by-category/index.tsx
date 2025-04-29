41c002d7dc84a56471dbfaaded3fbf4189d693b9
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-04-29 00:11:40 -0300

    feat: ensure the first page when changing category value

--- src/app/components/filter-by-category/index.tsx
+++ src/app/components/filter-by-category/index.tsx
@@ -9,28 +9,32 @@ export function FilterByCategory() {
     const {
         categoryValue,
         setCategoryValue,
+        setCurrentPage
     }  = useContext(AppContext) as AppContextProviderProps
 
-    function handleCategoryItem(categoryValue: Category) {
+    function handleCategoryItem(categoryValue: Category, contextCategoryValue: Category) {
+        if (contextCategoryValue !== categoryValue) {
+            setCurrentPage(0);
+        }
         setCategoryValue(categoryValue)
     }
 
     return (
         <CategoryListContainer>
             <CategoryItem
-                onClick={() => handleCategoryItem(Category.ALL_PRODUCTS)}
+                onClick={() => handleCategoryItem(Category.ALL_PRODUCTS, categoryValue)}
                 selected={categoryValue === Category.ALL_PRODUCTS ? true : false}
             >
                 todos os produtos
             </CategoryItem>
             <CategoryItem 
-                onClick={() => setCategoryValue(Category.TSHIRTS)}
+                onClick={() => handleCategoryItem(Category.TSHIRTS, categoryValue)}
                 selected={categoryValue === Category.TSHIRTS ? true : false}
             >
                 camisetas
             </CategoryItem>
             <CategoryItem 
-                onClick={() => setCategoryValue(Category.MUGS)}
+                onClick={() => handleCategoryItem(Category.MUGS, categoryValue)}
                 selected={categoryValue === Category.MUGS ? true : false}
             >
                 canecas
